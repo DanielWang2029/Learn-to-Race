3953028468b43f02304d80354ed7d6b34e3b73d4
DanielWang2029 <danielwangzt@gmail.com>
2023-12-20 01:25:01 -0500

    add test_main

--- scripts/test_main.py
+++ scripts/test_main.py
@@ -1,4 +1,5 @@
 from l2r import build_env
+from l2r.env_carla import RacingEnvCarla
 
 # from l2r import RacingEnv
 from src.config.yamlize import NameToSourcePath, create_configurable
@@ -8,38 +9,39 @@ import logging
 
 if __name__ == "__main__":
     # Build environment
-    env = build_env(
-        controller_kwargs={"quiet": True},
-        camera_cfg=[
-            {
-                "name": "CameraFrontRGB",
-                "Addr": "tcp://0.0.0.0:8008",
-                "Width": 512,
-                "Height": 384,
-                "sim_addr": "tcp://0.0.0.0:8008",
-            }
-        ],
-        env_kwargs={
-            "multimodal": True,
-            "eval_mode": True,
-            "n_eval_laps": 5,
-            "max_timesteps": 5000,
-            "obs_delay": 0.1,
-            "not_moving_timeout": 50000,
-            "reward_pol": "custom",
-            "provide_waypoints": False,
-            "active_sensors": ["CameraFrontRGB"],
-            "vehicle_params": False,
-        },
-        action_cfg={
-            "ip": "0.0.0.0",
-            "port": 7077,
-            "max_steer": 0.3,
-            "min_steer": -0.3,
-            "max_accel": 6,
-            "min_accel": -1,
-        },
-    )
+    # env = build_env(
+    #     controller_kwargs={"quiet": True},
+    #     camera_cfg=[
+    #         {
+    #             "name": "CameraFrontRGB",
+    #             "Addr": "tcp://0.0.0.0:8008",
+    #             "Width": 512,
+    #             "Height": 384,
+    #             "sim_addr": "tcp://0.0.0.0:8008",
+    #         }
+    #     ],
+    #     env_kwargs={
+    #         "multimodal": True,
+    #         "eval_mode": True,
+    #         "n_eval_laps": 5,
+    #         "max_timesteps": 5000,
+    #         "obs_delay": 0.1,
+    #         "not_moving_timeout": 50000,
+    #         "reward_pol": "custom",
+    #         "provide_waypoints": False,
+    #         "active_sensors": ["CameraFrontRGB"],
+    #         "vehicle_params": False,
+    #     },
+    #     action_cfg={
+    #         "ip": "0.0.0.0",
+    #         "port": 7077,
+    #         "max_steer": 0.3,
+    #         "min_steer": -0.3,
+    #         "max_accel": 6,
+    #         "min_accel": -1,
+    #     },
+    # )
+    env = RacingEnvCarla()
     runner = create_configurable(
         "config_files/random_sac/runner.yaml", NameToSourcePath.runner
     )
